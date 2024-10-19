cf26887a3ebea7228a3b6aa353b85e4a89ac0b7f
Khansalman786 <ks8591339148>
2024-10-19 21:37:24 +0530

    sample

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./components/NavBar";
+import Banner from "./components/Banner";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <NavBar />
+        <Banner />
+      </div>
+    </>
+  );
+};
+
+export default App;
